25ccc0c4820b342b8e523352876bd2f6898e74be
menjivarfrancowalterobed51-creator <menjivarfrancowalterobed51@gmail.com>
2026-04-13 11:33:07 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,19 @@
-// Service Worker para permitir la instalación de la PWA
-self.addEventListener('fetch', (event) => {});
+const CACHE_NAME = 'lumin-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/702/702797.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
